b4403963bd6fb5c879f2d990a36e5b846b9c8e44
TilavovD <tilavov.dostonbek@gmail.com>
2022-12-04 15:02:43 +0500

    day4 solutions added

---
+++ day4/part2.py
@@ -0,0 +1,25 @@
+from input import data
+data = data.split()
+
+def record_splitter(record):
+	record = record.split(",")
+	record1 = record[0]
+	record2 = record[1]
+	record1_start = record1.split("-")[0]
+	record1_end = record1.split("-")[1]
+	record2_start = record2.split("-")[0]
+	record2_end = record2.split("-")[1]
+	return record1_start, record1_end, record2_start, record2_end
+
+count = 0
+for row_index in range(len(data)):
+
+	record1_start, record1_end, record2_start, record2_end = record_splitter(data[row_index])
+
+	if int(record2_start) >= int(record1_start) and int(record2_start) <= int(record1_end) or int(record2_end) >= int(record1_start) and int(record2_end) <= int(record1_end): 
+		count += 1
+	elif int(record1_start) >= int(record2_start) and int(record1_start) <= int(record2_end) or int(record1_end) >= int(record2_start) and int(record1_end) <= int(record2_end):
+		print(record1_start, record1_end, record2_start, record2_end)
+		count += 1
+
+print(count)
